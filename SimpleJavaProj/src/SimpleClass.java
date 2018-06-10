394417f60ec840162f884effb35ec6387755533b
sapanprajapati <sapan.prajapati@gmail.com>
2018-06-10 20:11:46 +0530

    Changed commit message

--- SimpleJavaProj/src/SimpleClass.java
+++ SimpleJavaProj/src/SimpleClass.java
@@ -1,10 +1,8 @@
-
 public class SimpleClass {
 
 	public static void main(String[] args) {
 		System.out.println("First java statement");
-		int a=10,b=5;
+		int a = 10, b = 5;
 		System.out.println("Sum=" + new ArithmeticOperations().sum(a, b));
 	}
-
 }
